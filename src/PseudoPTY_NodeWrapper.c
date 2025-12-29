bfe0779e756b16d9ebd74c9352c42069bb8a5890
izhank216 <izhank216@gmail.com>
2025-12-29 12:57:53 +0500

    Create PseudoPTY_NodeWrapper.c

---
+++ src/PseudoPTY_NodeWrapper.c
@@ -0,0 +1,29 @@
+#include "WindowsPty.h"
+#include "Engine.h"
+#include "ProperInput.h"
+#include "VirtualTerminalProcessing.h"
+
+WindowsPty* PseudoPTY_Create(const char* command, int cols, int rows) {
+    enable_virtual_terminal();
+    return WindowsPty_Create(command, cols, rows);
+}
+
+void PseudoPTY_Destroy(WindowsPty* pty) {
+    WindowsPty_Destroy(pty);
+}
+
+int PseudoPTY_Write(WindowsPty* pty, const char* data, int len) {
+    return WindowsPty_Write(pty, data, len);
+}
+
+int PseudoPTY_Read(WindowsPty* pty, char* buffer, int len) {
+    return WindowsPty_Read(pty, buffer, len);
+}
+
+void PseudoPTY_Flush(WindowsPty* pty) {
+    flush_pty_output(PseudoPTY_Read, pty);
+}
+
+void PseudoPTY_Interactive(WindowsPty* pty) {
+    interactive_input(PseudoPTY_Write, PseudoPTY_Read, pty);
+}
