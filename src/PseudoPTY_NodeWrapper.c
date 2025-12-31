52fe2f4d518f775a0103cf8d5b7e75f462b6426c
izhank216 <izhank216@gmail.com>
2025-12-31 23:27:10 +0500

    Update PseudoPTY_NodeWrapper.c

--- src/PseudoPTY_NodeWrapper.c
+++ src/PseudoPTY_NodeWrapper.c
@@ -3,15 +3,38 @@
 #include "ProperInput.h"
 #include "VirtualTerminalProcessing.h"
 
-int PseudoPTY_Read(WindowsPty* pty, char* buffer, int len);
-int PseudoPTY_Write(WindowsPty* pty, const char* data, int len);
+// Original functions
+int PseudoPTY_Read(WindowsPty* pty, char* buffer, int len) {
+    return WindowsPty_Read(pty, buffer, len);
+}
 
+int PseudoPTY_Write(WindowsPty* pty, const char* data, int len) {
+    return WindowsPty_Write(pty, data, len);
+}
+
+// --- Wrappers for void* callbacks ---
+static int read_wrapper(void* userdata, char* buffer, int len) {
+    WindowsPty* pty = (WindowsPty*)userdata;
+    return PseudoPTY_Read(pty, buffer, len);
+}
+
+static int write_wrapper(void* userdata, const char* data, int len) {
+    WindowsPty* pty = (WindowsPty*)userdata;
+    return PseudoPTY_Write(pty, data, len);
+}
+
+static int read_wrapper2(void* userdata, char* buffer, int len) {
+    WindowsPty* pty = (WindowsPty*)userdata;
+    return PseudoPTY_Read(pty, buffer, len);
+}
+
+// --- Public API ---
 WindowsPty* PseudoPTY_Create(const char* command, int cols, int rows) {
     enable_virtual_terminal();
     WindowsPty* pty = WindowsPty_Create(command, cols, rows);
     if (pty) {
-        flush_pty_output(PseudoPTY_Read, pty);
-        interactive_input(PseudoPTY_Write, PseudoPTY_Read, pty);
+        flush_pty_output(read_wrapper, pty);
+        interactive_input(write_wrapper, read_wrapper2, pty);
     }
     return pty;
 }
@@ -20,18 +43,10 @@ void PseudoPTY_Destroy(WindowsPty* pty) {
     WindowsPty_Destroy(pty);
 }
 
-int PseudoPTY_Write(WindowsPty* pty, const char* data, int len) {
-    return WindowsPty_Write(pty, data, len);
-}
-
-int PseudoPTY_Read(WindowsPty* pty, char* buffer, int len) {
-    return WindowsPty_Read(pty, buffer, len);
-}
-
 void PseudoPTY_Flush(WindowsPty* pty) {
-    flush_pty_output(PseudoPTY_Read, pty);
+    flush_pty_output(read_wrapper, pty);
 }
 
 void PseudoPTY_Interactive(WindowsPty* pty) {
-    interactive_input(PseudoPTY_Write, PseudoPTY_Read, pty);
+    interactive_input(write_wrapper, read_wrapper2, pty);
 }
